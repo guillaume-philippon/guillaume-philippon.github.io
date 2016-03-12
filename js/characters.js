732cba6661bbf87ce4b989a2188976f45bb2330d
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2016-03-12 18:49:05 +0100

    Try to use collection

--- js/characters.js
+++ js/characters.js
@@ -1,10 +1,24 @@
 function load_character(name) {
     return $.getJSON('/characters/' + name + '.json', function(data){
+        /* Generic value */
         $('#name').html(data.name)
         $('#class').html(data.class)
+
+        /* Attributes */
+        $.each(data.attributes, function(attribute, value){
+            $('#'+attribute).html(value)
+        })
+
+        /* Bonus */
+
+        /* Skills */
+        $.each(data.skills, function(skill, value){
+            $.each(value, function(k, dice){
+                $('#'+skill+'-dice-'+k).css('color', dice)
+            })
+        })
     })
 }
 
 $(document).ready(function (){
-    sidebar_characters()
 })
