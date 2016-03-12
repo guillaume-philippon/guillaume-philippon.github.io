d55688bcc50d7f3f74e619a39f3ecea2657a2d3f
Guillaume PHILIPPON <guillaume.philippon@olympe-hn.org>
2016-03-12 18:25:34 +0100

    Tru to use collection

--- js/characters.js
+++ js/characters.js
@@ -1,16 +1,5 @@
-
-function sidebar_characters() {
-    return $.getJSON('/characters/?format=json', function(data) {
-        characters = ''
-        $.each(data.characters, function (idx, item){
-            characters += '<li><a href="#" onClick="load_character(\''+ item +'\')">' + item + '</a></li>'
-        })
-        $('#characters_sidebar').html(characters)
-    })
-};
-
 function load_character(name) {
-    return $.getJSON('/characters/' + name + '/?format=json', function(data){
+    return $.getJSON('/characters/' + name + '.json', function(data){
         $('#name').html(data.name)
         $('#class').html(data.class)
     })
